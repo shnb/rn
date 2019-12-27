7d8ffc477e6c8e819029fe6b8a182c55478f9527
宋林涛 <lintao.song@huifu.com>
2019-12-27 13:57:14 +0800

    新增装饰器注解功能

---
+++ rn-collie-proxy/Interceptor.js
@@ -0,0 +1,8 @@
+import BaseInterceptor from "rn-collie-proxy/BaseInterceptor";
+import {Proxy} from "rn-collie-proxy";
+
+export default (...interceptors: Array<BaseInterceptor>) => {
+    return function (target: any, methodName: string) {
+        Proxy.createObject(target).intercept().intercept(methodName, ...interceptors);
+    }
+}
