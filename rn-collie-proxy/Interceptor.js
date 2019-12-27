41397299e72ead8f786cb73a319e13c670cf9cc7
宋林涛 <lintao.song@huifu.com>
2019-12-27 13:58:01 +0800

    no message

--- rn-collie-proxy/Interceptor.js
+++ rn-collie-proxy/Interceptor.js
@@ -3,6 +3,6 @@ import {Proxy} from "rn-collie-proxy";
 
 export default (...interceptors: Array<BaseInterceptor>) => {
     return function (target: any, methodName: string) {
-        Proxy.createObject(target).intercept().intercept(methodName, ...interceptors);
+        Proxy.createObject(target).intercept(methodName, ...interceptors);
     }
 }
