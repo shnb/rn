64fe9c1afc471c205b09027c3b68d42638b70705
宋林涛 <lintao.song@huifu.com>
2019-12-31 15:41:36 +0800

    fix 导包

--- rn-collie-proxy/index.js
+++ rn-collie-proxy/index.js
@@ -1,10 +1,12 @@
 import BaseInterceptor from "./BaseInterceptor";
 import Proxy from "./Proxy";
 import MethodEntity from "./MethodEntity";
+import Interceptor from "./Interceptor";
 
 let exports = {
     Proxy,
     BaseInterceptor,
-    MethodEntity
+    MethodEntity,
+    Interceptor
 };
 module.exports = exports;
