e740f11d962049914b482a0f50dba22438a138c0
宋林涛 <lintao.song@huifu.com>
2020-01-17 14:47:48 +0800

    展开搜索组件增加动画支持

--- rn-collie/compoment/Expand/index.js
+++ rn-collie/compoment/Expand/index.js
@@ -1,5 +1,5 @@
 import React, {Component} from "react";
-import {TouchableWithoutFeedback, View} from "react-native";
+import {LayoutAnimation, Platform, TouchableWithoutFeedback, UIManager, View} from "react-native";
 
 type Props = {
     initOpen?: boolean,
@@ -19,18 +19,22 @@ export default class Expand extends Component<Props> {
         this.state = {
             ...this.state,
             ...props,
+        };
+        if (Platform.OS === 'android') {
+            UIManager.setLayoutAnimationEnabledExperimental(true);
         }
     }
+
+    changeLayout = () => {
+        LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut);
+        this.setState({open: !this.state.open});
+    };
 
     render() {
         let {title, content, initOpen, ...other} = this.props;
         let {open} = this.state;
         return <View {...other}>
-            <TouchableWithoutFeedback onPress={() => {
-                this.setState({
-                    open: !open,
-                })
-            }}>{title && title(open)}</TouchableWithoutFeedback>
+            <TouchableWithoutFeedback onPress={this.changeLayout}>{title && title(open)}</TouchableWithoutFeedback>
             {open && <View>{content && content(open)}</View>}
         </View>;
     }
